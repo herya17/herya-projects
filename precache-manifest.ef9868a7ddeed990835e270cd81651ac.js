9696b21a0afda1c070d0a7d371ca2184fee6326d
Heriyanto <67692606+herya17@users.noreply.github.com>
2022-10-03 01:17:36 +0700

    Herya Projects version 2

---
+++ precache-manifest.ef9868a7ddeed990835e270cd81651ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39248448613137c04abc",
+    "url": "/static/css/main.bb1cc63e.chunk.css"
+  },
+  {
+    "revision": "39248448613137c04abc",
+    "url": "/static/js/main.39248448.chunk.js"
+  },
+  {
+    "revision": "360777218696e8fafba7",
+    "url": "/static/js/1.36077721.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a898f3d18a47eee0a2b2d916dbe425d2",
+    "url": "/index.html"
+  }
+];
